b703e9eaa392ec5b417fc82cc085a7ca9a79527c
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-10-28 15:17:16 +0200

    adding PWA support

---
+++ serviceWorker.js
@@ -0,0 +1,29 @@
+const staticDashboard = "dashboard-v1";
+const assets = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/js/app.js",
+    "/favicon.ico",
+    "/app.html",
+    "/maplibre-gl.css",
+    "/maplibre-gl.js",
+    "/auth.js",
+    "/app.js",
+];
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticDashboard).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+});
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+});
